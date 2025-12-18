4661980127b9bd7e703475ffd9dcaa15f78742bb
TimWalter <tim.walter@tum.de>
2025-12-18 12:00:29 +0100

    ball baseline

---
+++ neural_capability_maps/baselines/model.py
@@ -0,0 +1,30 @@
+import json
+import re
+from pathlib import Path
+
+import torch
+import torch.nn as nn
+
+class Model(nn.Module):
+    @classmethod
+    def from_id(cls, model_id: int):
+        """
+        Instantiate a model from its wandb ID.
+
+        Args:
+            model_id: Wandb ID of the model.
+
+        Returns:
+            model: Instantiated model.
+        """
+
+        model_dir = Path(__file__).parent.parent.parent / "trained_models"
+        pattern = rf"[a-z]+-[a-z]+-{model_id}"
+        folder = next((f for f in model_dir.iterdir() if re.match(pattern, f.name)), None)
+        metadata_path = model_dir / folder / 'metadata.json'
+        metadata = json.load(open(metadata_path, 'r'))
+
+        model = cls(**metadata["model"])
+        model_folder = str(model_dir / folder)
+        model.load_state_dict(torch.load(next(Path(model_folder).glob('*.pth'))))
+        return model
