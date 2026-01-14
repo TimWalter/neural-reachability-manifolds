0ee8452db4fa03f5352ba44b8305f9b5628d3bb8
TimWalter <tim.walter@tum.de>
2026-01-14 10:19:48 +0100

    Clean up the training script and split of the logging part while also substantially improving it

---
+++ neural_capability_maps/logger.py
@@ -0,0 +1,211 @@
+import json
+from typing import Type
+from pathlib import Path
+
+import optuna
+import wandb
+import torch
+import numpy as np
+import seaborn as sns
+import plotly.graph_objects as go
+from jaxtyping import Float, Bool
+from torch import Tensor
+
+from data_sampling.representations import vector_to_homogeneous
+from neural_capability_maps.visualisation import get_pose_traces
+from neural_capability_maps.model import Model
+from neural_capability_maps.dataset import TrainingSet, ValidationSet
+
+
+class Logger:
+    def __init__(self, model_class: Type[Model],
+                 hyperparameter: dict,
+                 epochs: int,
+                 batch_size: int,
+                 early_stopping: int,
+                 lr: float,
+                 trial: optuna.Trial,
+                 training_set: TrainingSet,
+                 validation_set: ValidationSet,
+                 model: Model,
+                 loss_function: torch.nn.modules.loss._Loss
+                 ):
+        self.batch_size = batch_size
+        self.training_set = training_set
+        self.validation_set = validation_set
+        self.model = model
+        self.device = next(model.parameters()).device
+        self.loss_function = loss_function
+        metadata = {"num_training_samples": len(training_set) * batch_size,
+                    "num_validation_samples": len(validation_set) * batch_size,
+                    "model_class": model_class.__name__,
+                    "hyperparameter": hyperparameter,
+                    "epochs": epochs,
+                    "batch_size": batch_size,
+                    "early_stopping": early_stopping,
+                    "lr": lr}
+
+        self.run = self.setup_wandb(metadata, trial)
+        self.folder = self.save_metadata(metadata)
+
+        self.fix = list(validation_set.get_random_batch())
+        self.fix = [f.to(self.device, non_blocking=True) for f in self.fix]
+
+        self.buffer = {}
+
+    def setup_wandb(self, metadata: dict, trial: optuna.Trial) -> wandb.Run:
+        wandb.login(key="")
+        run = wandb.init(project="Capability Maps",
+                         config=metadata,
+                         group="Reachability",
+                         tags=[metadata["model_class"]])
+
+        if trial is not None:
+            run.name = f"trial/{trial.number}/{run.name}"
+        #wandb.watch(self.model, criterion=self.loss_function, log="all")
+
+        return run
+
+    def save_metadata(self, metadata: dict) -> Path:
+        folder = Path(__file__).parent.parent / "trained_models" / f"{self.run.name}"
+        Path(folder).mkdir(parents=True, exist_ok=True)
+        json.dump(metadata, open(folder / 'metadata.json', 'w'), indent=4)
+        return folder
+
+    def save_model(self):
+        torch.save(self.model.state_dict(), self.folder / "checkpoint.pth")
+
+    def __del__(self):
+        self.run.finish()
+
+    @staticmethod
+    def compute_metrics(logit: Float[Tensor, "batch"], label: Bool[Tensor, "batch"]) -> dict:
+        (true_positives, false_negatives), (false_positives, true_negatives) = binary_confusion_matrix(logit, label)
+        accuracy = true_positives / 2 + true_negatives / 2
+
+        hist, bin_edges = np.histogram(torch.nn.Sigmoid()(logit).cpu().numpy(), bins=64, range=(0.0, 1.0))
+
+        metrics = {'True Positives': true_positives,
+                   'True Negatives': true_negatives,
+                   'False Positives': false_positives,
+                   'False Negatives': false_negatives,
+                   'Accuracy': accuracy,
+                   'Predictions': wandb.Histogram(np_histogram=(hist, bin_edges)),
+                   }
+
+        return metrics
+
+    @staticmethod
+    def compute_input_metrics(morph: Float[Tensor, "batch dof 3"],
+                              pose: Float[Tensor, "batch 9"],
+                              label: Bool[Tensor, "batch"]) -> dict:
+        metrics = {"DOF": wandb.Histogram((morph != 0).any(dim=-1).sum(dim=-1).cpu().numpy() - 1)}
+
+        centre = torch.stack([
+            morph[:, 0, 1],
+            -morph[:, 0, 2] * torch.sin(morph[:, 0, 0]),
+            morph[:, 0, 2] * torch.cos(morph[:, 0, 0])], dim=-1)
+        metrics["Mean(Moveable Length)"] = torch.mean(1 - centre.norm(dim=1).cpu())
+
+        rel_pos = pose[:, :3] - centre
+        metrics["BaseDistance(Pose)"] = wandb.Histogram(rel_pos.norm(dim=1).cpu().numpy())
+
+        fig = go.Figure()
+        colors = sns.color_palette("colorblind", n_colors=2).as_hex()
+        for t in get_pose_traces(morph[label].cpu(), vector_to_homogeneous(pose[label]).cpu(), colors[0], "Reachable",
+                                 True):
+            fig.add_trace(t)
+        for t in get_pose_traces(morph[~label].cpu(), vector_to_homogeneous(pose[~label]).cpu(), colors[1],
+                                 "Unreachable", True):
+            fig.add_trace(t)
+        metrics["Poses"] = fig
+
+        metrics["Reachable [%]"] = label.sum().item() / label.shape[0]
+
+        return metrics
+
+    @staticmethod
+    def assign_space(data: dict, space: str) -> dict:
+        for key in list(data.keys()):
+            data[f"{space}/{key}"] = data.pop(key)
+        return data
+
+    @torch.no_grad()
+    def log_training(self,
+                     epoch: int,
+                     batch_idx: int,
+                     morph: Float[Tensor, "batch dof 3"],
+                     pose: Float[Tensor, "batch 9"],
+                     label: Bool[Tensor, "batch"],
+                     logit: Float[Tensor, "batch"],
+                     loss: float):
+        data = {"Loss": loss / self.batch_size, "Batch ID": self.training_set.current_batch_idx}
+        data |= self.compute_metrics(logit, label)
+        data = self.assign_space(data, "Training")
+
+        if batch_idx % 1000 == 0:
+            data |= self.assign_space(self.compute_input_metrics(morph, pose, label), "Input")
+        if batch_idx % 100 == 0:
+            logit = self.model(self.fix[1], self.fix[0])
+            loss = self.loss_function(logit, self.fix[2].float())
+
+            fix_data = {"Loss": loss}
+            fix_data |= self.compute_metrics(logit, self.fix[2])
+            fix_data = self.assign_space(fix_data, "Fix")
+            data |= fix_data
+
+        if batch_idx % 10 == 0:
+            morph, pose, label = self.validation_set.get_semi_random_batch()
+            morph = morph.to(self.device, non_blocking=True)
+            pose = pose.to(self.device, non_blocking=True)
+            label = label.to(self.device, non_blocking=True)
+
+            logit = self.model(pose, morph)
+            loss = self.loss_function(logit, label.float())
+
+            intermediate_val_data = {"Loss": loss}
+            intermediate_val_data |= self.compute_metrics(logit, label)
+            intermediate_val_data = self.assign_space(intermediate_val_data, "Intermediate Validation")
+            data |= intermediate_val_data
+
+        step = epoch * len(self.training_set) + batch_idx
+        self.run.log(data=data, step=step, commit=True)
+
+    @torch.no_grad()
+    def log_validation(self,
+                       epoch: int,
+                       batch_idx: int,
+                       morph: Float[Tensor, "batch dof 3"],
+                       pose: Float[Tensor, "batch 9"],
+                       label: Bool[Tensor, "batch"],
+                       logit: Float[Tensor, "batch"],
+                       loss: float):
+        if "loss" not in self.buffer:
+            self.buffer["loss"] = 0.0
+        if "logit" not in self.buffer:
+            self.buffer["logit"] = []
+        if "label" not in self.buffer:
+            self.buffer["label"] = []
+
+        self.buffer["loss"] += loss
+        self.buffer["logit"] += [logit.cpu()]
+        self.buffer["label"] += [label.cpu()]
+
+        if batch_idx + 1 == len(self.validation_set):
+            data = {"Loss": self.buffer["loss"] / len(self.validation_set) / self.batch_size}
+            data |= self.compute_metrics(torch.cat(self.buffer["logit"]), torch.cat(self.buffer["label"]))
+
+            step = (epoch + 1) * len(self.training_set)
+            self.run.log(data=self.assign_space(data, "Validation"), step=step, commit=False)
+            self.buffer = {}
+
+
+def binary_confusion_matrix(logit: Float[Tensor, "batch"], label: Bool[Tensor, "batch"]) \
+        -> Float[Tensor, "2 2"]:
+    predicted_label = torch.nn.Sigmoid()(logit) > 0.5
+    confusion_matrix = torch.zeros(2, 2)
+    confusion_matrix[0, 0] = (predicted_label & label).sum().item() / label.sum() * 100  # TP
+    confusion_matrix[0, 1] = (~predicted_label & label).sum().item() / label.sum() * 100  # FN
+    confusion_matrix[1, 0] = (predicted_label & ~label).sum().item() / (~label).sum() * 100  # FP
+    confusion_matrix[1, 1] = (~predicted_label & ~label).sum().item() / (~label).sum() * 100  # TN
+    return confusion_matrix
--- neural_capability_maps/train.py
+++ neural_capability_maps/train.py
@@ -1,198 +1,97 @@
 import argparse
-import json
+from typing import Type
 
-import wandb
 import torch
 import optuna
-import torch.optim as optim
 from tqdm import tqdm
-from pathlib import Path
-from neural_capability_maps.model import ReachabilityClassifier, SingleMorphologySIREN, MLP, TransTest
-from neural_capability_maps.dataset import Dataset, SingleDataset
-from data_sampling.representations import continuous_to_rotation_matrix, rotation_matrix_to_rotation_vector
+
+from neural_capability_maps.logger import Logger
+from neural_capability_maps.model import Model, OccupancyNetwork, Ball
+from neural_capability_maps.dataset import TrainingSet, ValidationSet
 
 
-
-def calculate_metrics(pred, labels):
-    predicted_labels = torch.nn.Sigmoid()(pred) > 0.5
-    labels = labels.bool()
-
-    true_positives = (predicted_labels & labels).sum().item() / labels.sum() * 100
-    true_negatives = (~predicted_labels & ~labels).sum().item() / (~labels).sum() * 100
-    false_positives = (predicted_labels & ~labels).sum().item() / (~labels).sum() * 100
-    false_negatives = (~predicted_labels & labels).sum().item() / labels.sum() * 100
-    accuracy = true_positives / 2 + true_negatives / 2
-
-    return true_positives, true_negatives, false_positives, false_negatives, accuracy
-
-def log_data(space, loss, pred, labels):
-    true_positives, true_negatives, false_positives, false_negatives, acc = calculate_metrics(pred, labels)
-
-    data = {f'{space}/Loss ': loss.item(),
-            f'{space}/True Positives': true_positives,
-            f'{space}/True Negatives': true_negatives,
-            f'{space}/False Positives': false_positives,
-            f'{space}/False Negatives': false_negatives,
-            f'{space}/Accuracy': acc,
-            f'{space}/Prediction Histogram': wandb.Histogram(torch.nn.Sigmoid()(pred).cpu().numpy())
-            }
-
-    return data
-
-def log_input_data(morph, pose, label):
-    data = {}
-    data[f'input/morph_alpha'] = wandb.Histogram(morph[..., 0].cpu().numpy())
-    data[f'input/morph_a'] = wandb.Histogram(morph[..., 1].cpu().numpy())
-    data[f'input/morph_d'] = wandb.Histogram(morph[..., 2].cpu().numpy())
-    data[f'input/translation_norm'] = wandb.Histogram(pose[:, :3].norm(dim=1).cpu().numpy())
-    axis_angle = rotation_matrix_to_rotation_vector(continuous_to_rotation_matrix(pose[:, 3:]))
-    data[f'input/rotation_angle0'] = wandb.Histogram(axis_angle[:, 0].cpu().numpy())
-    data[f'input/rotation_angle1'] = wandb.Histogram(axis_angle[:, 1].cpu().numpy())
-    data[f'input/rotation_angle2'] = wandb.Histogram(axis_angle[:, 2].cpu().numpy())
-    data[f'input/(reachable/total)'] = (label == 1).sum() / label.shape[0]
-    return data
-
-def main(epochs: int,
+def main(model_class: Type[Model],
+         hyperparameter: dict,
+         epochs: int,
          batch_size: int,
          early_stopping: int,
-         model_hyperparameter: dict = None,
-         optimiser_hyperparameter: dict = None,
-         trial: optuna.Trial = None
-         ):
-    if model_hyperparameter is None:
-        # model_hyperparameter = {
-        #     'encoder_config': {'width': 512, 'depth': 1},
-        #     'decoder_config': {'width': 128, 'depth': 4}
-        # }
-        model_hyperparameter = {
-            'encoder_config': {"nhead": 8, "dim_feedforward": 640, "dropout": 0.0},
-            'decoder_config': {"n_heads": 8, "mlp_dim": 640, "dropout": 0.0},
-            "latent_morph": 160, "latent_pose": 160,
-            "num_encoder_blocks": 4, "num_decoder_blocks": 4
-        }
-    if optimiser_hyperparameter is None:
-        optimiser_hyperparameter = {'lr': 6e-4}
-    settings = {'epochs': epochs, 'batch_size': batch_size, 'early_stopping': early_stopping}
-    settings['model_hyperparameter'] = model_hyperparameter
-    settings['optimiser_hyperparameter'] = optimiser_hyperparameter
-
+         lr: float,
+         trial: optuna.Trial = None):
     device = torch.device("cuda")
-    run = wandb.init(project="Capability Maps",
-                     config=settings if trial is None else {},
-                     group="reachability_classifier")
 
-    if trial is None:
-        folder = Path(__file__).parent.parent / "trained_models" / f"reachability_classifier_{run.name}"
-        Path(folder).mkdir(parents=True, exist_ok=True)
-        json.dump(settings, open(folder / 'settings.json', 'w'))
-    else:
-        run.name = f"trial/{trial.number}/{run.name}"
+    training_set = TrainingSet(batch_size, True)
+    validation_set = ValidationSet(batch_size, False)
 
-    training_set = SingleDataset(Path(__file__).parent.parent / 'data' / 'train', batch_size, False)
-    val_set = SingleDataset(Path(__file__).parent.parent / 'data' / 'val', batch_size, True)
-
-    model = TransTest().to(device)
-    #model = ReachabilityClassifier("transformer", **model_hyperparameter).to(device)
-    optimizer = optim.Adam(model.parameters(), **optimiser_hyperparameter)
-
+    model = model_class(**hyperparameter).to(device)
     loss_function = torch.nn.BCEWithLogitsLoss(reduction='mean')
 
-    wandb.watch(model, criterion=loss_function, log="all", log_graph=True)
+    optimizer = torch.optim.AdamW(model.parameters(), lr=lr)
 
-    fix_morph, fix_pose, fix_label = val_set[0]
-    fix_morph = fix_morph.to(device, non_blocking=True)
-    fix_pose = fix_pose.to(device, non_blocking=True)
-    fix_label = fix_label.to(device, non_blocking=True)
+    logger = Logger(model_class, hyperparameter, epochs, batch_size, early_stopping, lr, trial, training_set,
+                    validation_set, model, loss_function)
 
     min_loss = torch.inf
     early_stopping_counter = 0
     for e in range(epochs):
+        model.train()
         for batch_idx, (morph, pose, label) in enumerate(tqdm(training_set, desc=f"Training")):
-            step = e * len(training_set) + batch_idx
-
             morph = morph.to(device, non_blocking=True)
             pose = pose.to(device, non_blocking=True)
             label = label.to(device, non_blocking=True)
 
-            model.train()
             model.zero_grad()
-            if batch_idx % 100 == 0:
-                run.log(data=log_input_data(morph, pose, label), step=step, commit=False)
-            pred = model(pose, morph)
-            loss = loss_function(pred, label)
+
+            logit = model(pose, morph)
+            loss = loss_function(logit, label.float())
             loss.backward()
+
             torch.nn.utils.clip_grad_norm_(model.parameters(), max_norm=1.0)
             optimizer.step()
 
-            val_morph, val_pose, val_label = val_set.get_random_batch()
-            val_morph = val_morph.to(device, non_blocking=True)
-            val_pose = val_pose.to(device, non_blocking=True)
-            val_label = val_label.to(device, non_blocking=True)
+            logger.log_training(e, batch_idx, morph, pose, label, logit, loss)
 
-            model.eval()
+        model.eval()
+        loss = 0.0
+        for batch_idx, (morph, pose, label) in enumerate(tqdm(validation_set, desc=f"Training")):
+            morph = morph.to(device, non_blocking=True)
+            pose = pose.to(device, non_blocking=True)
+            label = label.to(device, non_blocking=True)
+
             with torch.no_grad():
-                val_pred = model(val_pose, val_morph)
-                val_loss = loss_function(val_pred, val_label)
+                logit = model(pose, morph)
+                loss += loss_function(logit, label.float())
+            logger.log_validation(e, batch_idx, morph, pose, label, logit, loss)
+        loss /= len(validation_set) * batch_size
 
-                if batch_idx % 10 == 0:
-                    fix_pred = model(fix_pose, fix_morph)
-                    fix_loss = loss_function(fix_pred, fix_label)
-                    run.log(data=log_data('fix', fix_loss, fix_pred, fix_label), step=step, commit=False)
+        if trial is not None:
+            trial.report(loss, e)
+            if trial.should_prune():
+                raise optuna.TrialPruned()
 
-                run.log(data=log_data('train', loss, pred, label), step=step, commit=False)
-                run.log(data=log_data('val', val_loss, val_pred, val_label), step=step, commit=True)
-            if trial is not None:
-                trial.report(val_loss, step)
-                if trial.should_prune():
-                    run.finish()
-                    raise optuna.TrialPruned()
+        if loss < min_loss:
+            min_loss = loss
+            early_stopping_counter = 0
+            logger.save_model()
+        else:
+            early_stopping_counter += 1
+            if early_stopping_counter == early_stopping:
+                (print('Early Stopping'))
+                return min_loss
 
-            if val_loss < min_loss:
-                min_loss = val_loss
-                early_stopping_counter = 0
-                if trial is None:
-                    torch.save(model.state_dict(), folder / "reachability_classifier.pth")
-            else:
-                early_stopping_counter += 1
-                if early_stopping_counter == early_stopping:
-                    (print('Early Stopping'))
-                    run.finish()
-                    return min_loss
-            if batch_idx == 500:
-                break
-
-        # preds = []
-        # losses = []
-        # labels = []
-        # for val_morph, val_pose, val_label in tqdm(val_set, desc=f"Validation"):
-        #     val_morph = val_morph.to(device, non_blocking=True)
-        #     val_pose = val_pose.to(device, non_blocking=True)
-        #     val_label = val_label.to(device, non_blocking=True)
-        #     model.eval()
-        #     with torch.no_grad():
-        #         val_pred = model(val_pose, val_morph * 0)
-        #         val_loss = loss_function(val_pred, val_label)
-        #     preds.append(val_pred.cpu())
-        #     losses.append(val_loss.item())
-        #     labels.append(val_label.cpu())
-        # preds = torch.cat(preds, dim=0)
-        # labels = torch.cat(labels, dim=0)
-        # losses = torch.tensor(losses).mean()
-        # run.log(data=log_data('full_val', losses, preds, labels), step=e * len(training_set) + batch_idx + 1, commit=False)
-    torch.save(model.state_dict(), folder / "reachability_classifier.pth")
-    run.finish()
     return min_loss
 
 
 if __name__ == '__main__':
-    wandb.login(key="")
     torch.manual_seed(0)
 
     parser = argparse.ArgumentParser()
-    parser.add_argument("--epochs", type=int, default=200)
-    parser.add_argument("--batch_size", type=int, default=2000)
+    parser.add_argument("--model_class", type=str, default="OccupancyNetwork")
+    parser.add_argument("--epochs", type=int, default=100)
+    parser.add_argument("--batch_size", type=int, default=1000)
     parser.add_argument("--early_stopping", type=int, default=-1)
+    parser.add_argument("--lr", type=float, default=3e-4)
 
     args = parser.parse_args()
-    kwargs = vars(args)
-    main(**kwargs)
+    args.model_class = eval(args.model_class)
+
+    main(**vars(args), hyperparameter={"encoder_config": {"dim_encoding": 16}, "decoder_config": {}})
